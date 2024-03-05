de49c3e76fa71e94a049cf7e3f9c98d2840e07f9
Pedro Nofuentes <pedro@fluidpayments.io>
2024-03-05 14:58:08 +0100

    Changed to use the _render_ way in React 17

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { render } from 'react-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const appContainer = document.getElementById('root');
+render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    appContainer
+);
