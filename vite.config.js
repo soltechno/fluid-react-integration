1e573258503bed71ee3296adae7f9a064fc22185
Carlos <carlos@soltechnologies.es>
2024-07-08 15:12:23 +0200

    revert https server

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import * as fs from 'fs';
+// import * as fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-      host: 'local.fluid-react.com',
-      https: {
-          key: fs.readFileSync('./.certs/server.key'),
-          cert: fs.readFileSync('./.certs/server.crt'),
-      },
-      port: 7227,
-  }
+//   server: {
+//       host: 'local.fluid-react.com',
+//       https: {
+//           key: fs.readFileSync('./.certs/server.key'),
+//           cert: fs.readFileSync('./.certs/server.crt'),
+//       },
+//       port: 7227,
+//   }
 })
