57120250be98dc8de2ee0647587c37e6ccd615a5
Carlos <carlos@soltechnologies.es>
2024-07-08 14:59:13 +0200

    add certs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+      host: 'local.fluid-react.com',
+      https: {
+          key: fs.readFileSync('./.certs/server.key'),
+          cert: fs.readFileSync('./.certs/server.crt'),
+      },
+      port: 7227,
+  }
 })
