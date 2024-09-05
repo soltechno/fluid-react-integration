885d93617474d6f4aace1ac9316755ae113f1880
Pedro Nofuentes <pedro@fluidpayments.io>
2024-09-05 15:04:44 +0200

    Added handy widget initialisation

---
+++ src/widget.js
@@ -0,0 +1,12 @@
+export default {
+	userId: 'user1',
+	sessionId: '10001-a-valid-session',
+	locale: 'en-EN',
+	countryCode: 'IRL',
+	currencyCode: 'EUR',
+	userData: {
+		age: 33,
+		previousDeposit: 125,
+		fullName: 'Peter Parker'
+	}
+};
